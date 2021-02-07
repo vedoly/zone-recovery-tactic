976aeb2544255fc255c882d2666f73e00054c853
velodica <ravipas.apk@gmail.com>
2021-02-07 15:41:47 +0700

    refactor

--- zone_recovery/zone.py
+++ zone_recovery/zone.py
@@ -11,16 +11,39 @@ class Zone:
         self.up_distance = above_target-up
         self.down_distance = below_target-down
 
-    def calculateProfit(self, t, df, modulation):
+    def calculateProfit(self, t, df, money):
+        state = 'Sell'
+        first_money = money
+        buy_cost = 0
+        sell_cost = 0
 
-        open = []
-        hold = []
+        buy_unit = 0
+        sell_unit = 0
 
-        first_price = df.iloc[0].Price
-        open.append(first_price)
+        for i, row in df.iterrows():
 
-        for i, row in df[1:].iterrows():
-            if row.Price > 0:
-                pass
+            if row.Price > buy_cost and state == 'Buy':
+                buy_cost += 50
+                money -= buy_cost
+                buy_unit += buy_cost / row.Price
+                state = 'Sell'
 
-        return t, df.iloc[0].Price, modulation
+            if row.Price > sell_cost and state == 'Sell':
+                sell_cost += 50
+                money -= sell_cost
+                sell_unit += sell_cost / row.Price
+                state = 'Buy'
+
+            if row.Price > self.above_target and state == 'Sell':
+                buy_cost = 0
+                money += buy_unit * row.Price
+                buy_unit = 0
+                state = 'Buy'
+
+            if row.Price > self.above_target and state == 'Buy':
+                sell_cost = 0
+                money += sell_unit * row.Price
+                sell_unit = 0
+                state = 'Sell'
+
+        return money - first_money
