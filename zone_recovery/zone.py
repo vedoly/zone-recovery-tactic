9f520287124e3cfa41849c7f4ff160795b0c7872
velodica <ravipas.apk@gmail.com>
2021-02-08 15:18:27 +0700

    refactor

--- zone_recovery/zone.py
+++ zone_recovery/zone.py
@@ -2,14 +2,12 @@ import numpy as numpy
 
 
 class Zone:
-    def __init__(self, up, down, above_target, below_target):
-        self.up = up
-        self.down = down
-        self.above_target = above_target
-        self.below_target = below_target
-        self.zone_width = up-down
-        self.up_distance = above_target-up
-        self.down_distance = below_target-down
+    def __init__(self, entry_price, zone_width, distance):
+        "entry_price"
+        self.up = entry_price
+        self.down = entry_price - zone_width
+        self.above_target = entry_price + distance
+        self.below_target = entry_price - zone_width - distance
 
     def calculateProfit(self, t, df, money):
         state = 'Sell'
@@ -31,6 +29,7 @@ class Zone:
                 money -= buy_cost
                 buy_unit += buy_cost / row.Price
                 state = 'Sell'
+                print('Buy', buy_cost)
 
             if row.Price > sell_cost and state == 'Sell':
                 sell_cost += 50 * alpha
@@ -40,18 +39,27 @@ class Zone:
                 money -= sell_cost
                 sell_unit += sell_cost / row.Price
                 state = 'Buy'
+                print('Sell', sell_cost)
 
             if row.Price > self.above_target and state == 'Sell':
                 buy_cost = 0
                 money += buy_unit * row.Price
-                buy_unit = 0
-                state = 'Buy'
 
-            if row.Price > self.above_target and state == 'Buy':
+                state = 'Buy'
+                print('Close Buy', buy_unit * row.Price)
+                buy_unit = 0
+                alpha = 1
+
+            if row.Price < self.below_target and state == 'Buy':
                 sell_cost = 0
                 debt = sell_unit * row.Price
                 money += temp_money - debt
                 sell_unit = 0
                 state = 'Sell'
+                print('Close Sell', temp_money - debt)
+                alpha = 1
 
         return money - first_money
+
+    def monitorZone(self):
+        return self.up, self.down, self.above_target, self.below_target
