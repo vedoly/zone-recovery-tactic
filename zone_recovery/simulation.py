f007673a12e90f55110a1b517020aebe0fa3e3d0
velodica <ravipas.apk@gmail.com>
2021-02-11 16:48:15 +0700

    fix bugs and refactor

--- zone_recovery/simulation.py
+++ zone_recovery/simulation.py
@@ -13,40 +13,86 @@ def makeDecision(prev_price, current_price, zone: Zone, state: str):
         return 'Close Buy', 'w8 4 Buy'
 
     if (prev_price > zone.below_target and current_price < zone.below_target or current_price == zone.below_target) and state == 'w8 4 Buy':
-        return 'Close Sell', 'w8 4 Sell'
+        return 'Close Sell', 'w8 4 Buy'
 
     return 'Steady', state
 
 
-def simulation(df, limit=4, alpha=2, money=1000):
+def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, money=100, zone_width=1, distance=4):
 
-    state = 'w8 4 Sell'
-    action = 'Open Buy'
-    first_money = money
-    temp_money = 0
-    buy_cost = 1
-    sell_cost = 0
+    buy_unit, sell_unit, buy_hold, sell_hold = 0, 0, 0, 0
+    alpha = 1
 
-    sell_unit = 0
+    log = []
+    for i, current in df.iterrows():
 
-    first_price = df.iloc[0].Price
-    prev_price = first_price
-    buy_unit = buy_cost / first_price
+        if buy_hold == 0 and sell_hold == 0:
+            zone = Zone(current.Price, current.Price *
+                        zone_width / 100, current.Price * distance / 100)
+            state, action = 'w8 4 Sell', 'Open Buy'
 
-    money -= buy_cost
-    print(action, first_price, money, 1, 0)
+        else:
+            action, state = makeDecision(
+                prev_price, current.Price, zone, state)
 
-    zone = Zone(first_price, first_price * 0.005, first_price * 0.02)
+        if entry_amount * alpha > money:
+            action = 'Cut Loss'
 
-    buy_hold = 1
-    sell_hold = 0
+        if action == 'Open Buy':
+            buy_hold += entry_amount*alpha
+            money -= entry_amount*alpha
+            buy_unit += entry_amount*alpha / current.Price
+            alpha *= multiplier
 
-    for i, row in df[1:].iterrows():
+        if action == 'Open Sell':
+            sell_hold += entry_amount*alpha
+            money -= entry_amount*alpha
+            sell_unit += entry_amount*alpha / current.Price
+            alpha *= multiplier
+
+        if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
+            money += buy_unit * current.Price + 2*sell_hold - sell_unit * current.Price
+            buy_unit = 0
+            sell_unit = 0
+            buy_hold, sell_hold = 0, 0
+            alpha = 1
+            state = 'w8 4 Sell'
+
+        if(action != 'Steady'):
+            print(action, current.Price, money, buy_hold, sell_hold)
+
+        log.append((action, money, buy_hold, sell_hold))
+        prev_price = current.Price
+    return log
+
+
+def simulation0(df, limit=0, expected_profit=4, alpha=1, money=10, zone_width=1, distance=4):
+
+    money = limit
+
+    temp_money, buy_cost, sell_cost = 0, 0, 0
+
+    buy_unit, sell_unit, buy_hold, sell_hold = 0, 0, 0, 0
+
+    profit = 0
+
+    for i, row in df.iterrows():
 
         current_price = row.Price
-        action, state = makeDecision(prev_price, current_price, zone, state)
 
-        if alpha > 32:
+        if buy_hold == 0 and sell_hold == 0:
+            zone = Zone(current_price, current_price *
+                        zone_width / 100, current_price * distance / 100)
+            state, action = 'w8 4 Sell', 'Open Buy'
+
+        else:
+            action, state = makeDecision(
+                prev_price, current_price, zone, state)
+
+        if buy_hold + sell_hold > limit:
+            action, state = 'Cut Loss', 'w8 4 Sell'
+
+        if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
 
             buy_cost = 0
             money += buy_unit * row.Price
@@ -54,14 +100,16 @@ def simulation(df, limit=4, alpha=2, money=1000):
 
             sell_cost = 0
             debt = sell_unit * row.Price
-            money += temp_money - debt
+            money += 2*temp_money - debt
             sell_unit = 0
             buy_hold, sell_hold = 0, 0
             alpha = 1
             temp_money = 0
-            action = 'Cut Loss'
 
         if action == 'Open Buy':
+            if buy_hold + sell_hold + alpha > limit:
+                action = 'Cut Loss'
+                pass
             buy_cost += 1 * alpha
             buy_hold += alpha
             alpha *= 2
@@ -72,6 +120,10 @@ def simulation(df, limit=4, alpha=2, money=1000):
             # print('Open Buy', buy_cost, row.Price,money)
 
         if action == 'Open Sell':
+            if buy_hold + sell_hold + alpha > limit:
+                action = 'Cut Loss'
+                pass
+
             sell_cost += 1 * alpha
             sell_hold += alpha
             temp_money += sell_cost
@@ -79,39 +131,28 @@ def simulation(df, limit=4, alpha=2, money=1000):
             money -= sell_cost
             sell_unit += sell_cost / row.Price
 
-            # state = 'Buy'
-            # print('Open Sell', sell_cost,money)
-
-        if action == 'Close Buy':
+        if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
 
             buy_cost = 0
             money += buy_unit * row.Price
-
-            # state = 'Buy'
-            # print('Close Buy', buy_unit * row.Price,money)
-            buy_hold = 0
             buy_unit = 0
-            alpha = 1
-
-        if action == 'Close Sell':
 
             sell_cost = 0
             debt = sell_unit * row.Price
-            print(temp_money, debt)
             money += 2*temp_money - debt
             sell_unit = 0
-            sell_hold = 0
-            temp_money = 0
-            # state = 'Sell'
-            # print('Close Sell', temp_money - debt,money)
+            buy_hold, sell_hold = 0, 0
             alpha = 1
+            temp_money = 0
 
-        if sell_unit == 0 and buy_unit == 0:
-            zone = Zone(current_price, current_price *
-                        0.005, current_price * 0.02)
+            # state = 'Buy'
+            # print('Open Sell', sell_cost,money)
+
+        # if sell_unit == 0 and buy_unit == 0:
+        #     zone = Zone(current_price,current_price* zone_width / 100, current_price* distance / 100)
 
         if(action != 'Steady'):
             print(action, current_price, money, buy_hold, sell_hold)
 
         prev_price = current_price
-    print(money-first_money)
+    return money
