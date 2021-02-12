896f7d5e16f89e2ef105ef59a656fcf9cce1c2ee
velodica <ravipas.apk@gmail.com>
2021-02-12 17:01:37 +0700

    add leverage

--- zone_recovery/simulation.py
+++ zone_recovery/simulation.py
@@ -18,141 +18,91 @@ def makeDecision(prev_price, current_price, zone: Zone, state: str):
     return 'Steady', state
 
 
-def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, money=100, zone_width=1, distance=4):
+def changeValue(holds, units, current_Price, state, leverage):
+    out = holds.copy()
+    for i in range(len(holds)):
+        out[i] += (units[i]*current_Price-holds[i]) * state * leverage
+    return out
 
-    buy_unit, sell_unit, buy_hold, sell_hold = 0, 0, 0, 0
-    alpha = 1
+
+def caculateNextTradeAmount(expected_profit, entry_amount, buy_hold, sell_hold, zone_width=1, distance=4):
+
+    expected_profit = (expected_profit) / 100
+    current_option_hold, opposit_option_hold = min(
+        buy_hold, sell_hold), max(buy_hold, sell_hold)
+
+    if buy_hold + sell_hold == 0:
+        return entry_amount
+
+    return ((opposit_option_hold) * (zone_width/100 + distance/100) + expected_profit) / (distance/100) - current_option_hold
+
+
+def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, money=100, zone_width=1, distance=4, leverage=1):
+
+    buy_unit, sell_unit, buy_hold, sell_hold = list(), list(), list(), list()
+    buy_current, sell_current = list(), list()
+    alpha = entry_amount
+    first_money = money
+    count = 0
 
     log = []
     for i, current in df.iterrows():
+        if(money + sum(buy_current+[0]) + sum(sell_current+[0])) < 0.3 * first_money:
+            print(action, current.Price, money, buy_current, sell_current,
+                  money + sum(buy_current+[0]) + sum(sell_current+[0]))
 
-        if buy_hold == 0 and sell_hold == 0:
+            print("Broken")
+            return
+        if len(buy_hold) == 0 and len(sell_hold) == 0:
             zone = Zone(current.Price, current.Price *
                         zone_width / 100, current.Price * distance / 100)
             state, action = 'w8 4 Sell', 'Open Buy'
-
+            alpha = entry_amount
         else:
             action, state = makeDecision(
                 prev_price, current.Price, zone, state)
 
-        if entry_amount * alpha > money:
+            if action != 'Steady':
+                alpha = caculateNextTradeAmount(expected_profit, entry_amount, sum(
+                    buy_hold), sum(sell_hold), zone_width, distance)
+
+        if alpha > money or count >= limit:
             action = 'Cut Loss'
 
         if action == 'Open Buy':
-            buy_hold += entry_amount*alpha
-            money -= entry_amount*alpha
-            buy_unit += entry_amount*alpha / current.Price
-            alpha *= multiplier
+            buy_hold.append(alpha)
+            money -= alpha
+            buy_unit.append(alpha / current.Price)
+            count += 1
+            # buy_price.append(current.Price)
 
         if action == 'Open Sell':
-            sell_hold += entry_amount*alpha
-            money -= entry_amount*alpha
-            sell_unit += entry_amount*alpha / current.Price
-            alpha *= multiplier
+            sell_hold.append(alpha)
+            money -= alpha
+            sell_unit.append(alpha / current.Price)
+            count += 1
+            # sell_price.append(current.Price)
+
+        buy_current = changeValue(
+            buy_hold, buy_unit, current.Price, 1, leverage)
+        sell_current = changeValue(
+            sell_hold, sell_unit, current.Price, -1, leverage)
 
         if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
-            money += buy_unit * current.Price + 2*sell_hold - sell_unit * current.Price
-            buy_unit = 0
-            sell_unit = 0
-            buy_hold, sell_hold = 0, 0
-            alpha = 1
+            money += sum(buy_current+[0]) + sum(sell_current+[0])
+            buy_unit = []
+            sell_unit = []
+            buy_hold, sell_hold = [], []
+            buy_current, sell_current = list(), list()
+            alpha = entry_amount
             state = 'w8 4 Sell'
+            count = 0
 
         if(action != 'Steady'):
-            print(action, current.Price, money, buy_hold, sell_hold)
+            print(action, current.Price, money, buy_current, sell_current,
+                  money + sum(buy_current+[0]) + sum(sell_current+[0]))
 
-        log.append((action, money, buy_hold, sell_hold))
+            log.append((action, money, buy_current, sell_current))
         prev_price = current.Price
+    print(money)
     return log
-
-
-def simulation0(df, limit=0, expected_profit=4, alpha=1, money=10, zone_width=1, distance=4):
-
-    money = limit
-
-    temp_money, buy_cost, sell_cost = 0, 0, 0
-
-    buy_unit, sell_unit, buy_hold, sell_hold = 0, 0, 0, 0
-
-    profit = 0
-
-    for i, row in df.iterrows():
-
-        current_price = row.Price
-
-        if buy_hold == 0 and sell_hold == 0:
-            zone = Zone(current_price, current_price *
-                        zone_width / 100, current_price * distance / 100)
-            state, action = 'w8 4 Sell', 'Open Buy'
-
-        else:
-            action, state = makeDecision(
-                prev_price, current_price, zone, state)
-
-        if buy_hold + sell_hold > limit:
-            action, state = 'Cut Loss', 'w8 4 Sell'
-
-        if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
-
-            buy_cost = 0
-            money += buy_unit * row.Price
-            buy_unit = 0
-
-            sell_cost = 0
-            debt = sell_unit * row.Price
-            money += 2*temp_money - debt
-            sell_unit = 0
-            buy_hold, sell_hold = 0, 0
-            alpha = 1
-            temp_money = 0
-
-        if action == 'Open Buy':
-            if buy_hold + sell_hold + alpha > limit:
-                action = 'Cut Loss'
-                pass
-            buy_cost += 1 * alpha
-            buy_hold += alpha
-            alpha *= 2
-            money -= buy_cost
-            buy_unit += buy_cost / row.Price
-
-            # state = 'Sell'
-            # print('Open Buy', buy_cost, row.Price,money)
-
-        if action == 'Open Sell':
-            if buy_hold + sell_hold + alpha > limit:
-                action = 'Cut Loss'
-                pass
-
-            sell_cost += 1 * alpha
-            sell_hold += alpha
-            temp_money += sell_cost
-            alpha *= 2
-            money -= sell_cost
-            sell_unit += sell_cost / row.Price
-
-        if action == 'Cut Loss' or action == 'Close Buy' or action == 'Close Sell':
-
-            buy_cost = 0
-            money += buy_unit * row.Price
-            buy_unit = 0
-
-            sell_cost = 0
-            debt = sell_unit * row.Price
-            money += 2*temp_money - debt
-            sell_unit = 0
-            buy_hold, sell_hold = 0, 0
-            alpha = 1
-            temp_money = 0
-
-            # state = 'Buy'
-            # print('Open Sell', sell_cost,money)
-
-        # if sell_unit == 0 and buy_unit == 0:
-        #     zone = Zone(current_price,current_price* zone_width / 100, current_price* distance / 100)
-
-        if(action != 'Steady'):
-            print(action, current_price, money, buy_hold, sell_hold)
-
-        prev_price = current_price
-    return money
