5e9adb4b7a263d6d2312f5a6042fb7709f80a96a
velodica <ravipas.apk@gmail.com>
2021-02-13 00:21:09 +0700

    :bug: fix entry exit condition

--- zone_recovery/simulation.py
+++ zone_recovery/simulation.py
@@ -1,7 +1,7 @@
-from zone import Zone
+from zone_recovery.zone import Zone
 
 
-def makeDecision(prev_price, current_price, zone: Zone, state: str):
+def makeDecision0(prev_price, current_price, zone: Zone, state: str):
 
     if (prev_price > zone.up and current_price < zone.up or current_price == zone.up) and state == 'w8 4 Buy':
         return 'Open Buy', 'w8 4 Sell'
@@ -14,6 +14,23 @@ def makeDecision(prev_price, current_price, zone: Zone, state: str):
 
     if (prev_price > zone.below_target and current_price < zone.below_target or current_price == zone.below_target) and state == 'w8 4 Buy':
         return 'Close Sell', 'w8 4 Buy'
+
+    return 'Steady', state
+
+
+def makeDecision(prev_price, current_price, zone: Zone, state: str):
+
+    if (prev_price < zone.below_target and current_price > zone.below_target or current_price == zone.below_target) and state == 'w8 4 Buy':
+        return 'Close Sell', 'w8 4 Buy'
+
+    if (prev_price > zone.above_target and current_price < zone.above_target or current_price == zone.above_target) and state == 'w8 4 Sell':
+        return 'Close Buy', 'w8 4 Buy'
+
+    if (prev_price > zone.up and current_price < zone.up or current_price == zone.up) and state == 'w8 4 Buy':
+        return 'Open Buy', 'w8 4 Sell'
+
+    if (prev_price < zone.down and current_price > zone.down or current_price == zone.down) and state == 'w8 4 Sell':
+        return 'Open Sell', 'w8 4 Buy'
 
     return 'Steady', state
 
@@ -37,22 +54,25 @@ def caculateNextTradeAmount(expected_profit, entry_amount, buy_hold, sell_hold, 
     return ((opposit_option_hold) * (zone_width/100 + distance/100) + expected_profit) / (distance/100) - current_option_hold
 
 
-def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, money=100, zone_width=1, distance=4, leverage=1):
+def simulation(df, entry_amount, open_limit=99, expected_profit=4, multiplier=2, money=100, zone_width=1, distance=4, leverage=1, guarantee_limit=0.3):
 
     buy_unit, sell_unit, buy_hold, sell_hold = list(), list(), list(), list()
     buy_current, sell_current = list(), list()
     alpha = entry_amount
     first_money = money
     count = 0
-
+    mininum_balance = money
     log = []
+    print('Simulating ...')
     for i, current in df.iterrows():
-        if(money + sum(buy_current+[0]) + sum(sell_current+[0])) < 0.3 * first_money:
+        # if i % 10000 == 0:
+        #     print('.', end='')
+        if(money + sum(buy_current+[0]) + sum(sell_current+[0])) < guarantee_limit * first_money:
             print(action, current.Price, money, buy_current, sell_current,
                   money + sum(buy_current+[0]) + sum(sell_current+[0]))
 
             print("Broken")
-            return
+            return log
         if len(buy_hold) == 0 and len(sell_hold) == 0:
             zone = Zone(current.Price, current.Price *
                         zone_width / 100, current.Price * distance / 100)
@@ -66,7 +86,10 @@ def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, mone
                 alpha = caculateNextTradeAmount(expected_profit, entry_amount, sum(
                     buy_hold), sum(sell_hold), zone_width, distance)
 
-        if alpha > money or count >= limit:
+        if alpha > money:
+            action = 'Cut Loss'
+
+        if action[:4] == 'Open' and count >= open_limit:
             action = 'Cut Loss'
 
         if action == 'Open Buy':
@@ -97,12 +120,14 @@ def simulation(df, entry_amount, limit=99, expected_profit=4, multiplier=2, mone
             alpha = entry_amount
             state = 'w8 4 Sell'
             count = 0
-
+        mininum_balance = min(mininum_balance, money +
+                              sum(buy_current+[0]) + sum(sell_current+[0]))
         if(action != 'Steady'):
             print(action, current.Price, money, buy_current, sell_current,
                   money + sum(buy_current+[0]) + sum(sell_current+[0]))
 
-            log.append((action, money, buy_current, sell_current))
+        log.append((action, current.Price, money, buy_current, sell_current,
+                    money + sum(buy_current+[0]) + sum(sell_current+[0]), current.Month, current.Day))
         prev_price = current.Price
-    print(money)
+    print(money, mininum_balance)
     return log
